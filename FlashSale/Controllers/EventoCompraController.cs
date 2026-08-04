6084552ecc5145ef6c5c8f1fb0d5e247c1433e88
Jobyn Macías <jobyn_mejia@hotmail.com>
2026-08-03 18:03:30 -0600

    Cambios de codigo en Controladores y en el cs de eventoCompra

--- FlashSale/Controllers/EventoCompraController.cs
+++ FlashSale/Controllers/EventoCompraController.cs
@@ -14,8 +14,40 @@ namespace FlashSale.Controllers
             _service = service;
         }
 
+        [HttpGet]
         public IActionResult Generador()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Generador(GeneradorMasivoDTO dto)
+        {
+            for (int i = 0; i < dto.CantidadEventos; i++)
+            {
+                EventoCompra evento = new EventoCompra();
+
+                evento.Id = Guid.NewGuid().ToString();
+
+                evento.ProductoId = dto.ProductoId;
+
+                evento.ClienteId = "CLI-" + Random.Shared.Next(1000, 9999);
+
+                evento.Cantidad = dto.CantidadPorCompra;
+
+                evento.Precio = 950;
+
+                evento.Categoria = "Electrónica";
+
+                evento.FechaHora = DateTime.Now;
+
+                evento.Estado = "Pendiente";
+
+                await _service.Agregar(evento);
+            }
+
+            ViewBag.Mensaje = "Eventos generados correctamente.";
+
             return View();
         }
     }
